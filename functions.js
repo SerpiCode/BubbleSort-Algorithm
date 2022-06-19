c90f0772827dfa3a52a6ffdf6412c9c59abc5d04
Luis Henrique <94150246+SerpiCode@users.noreply.github.com>
2022-06-19 17:14:22 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,9 +1,90 @@
 $(document).ready(function()
 {
-  var generate = $("#generate");
-  var sort = $("#sort");
+  // #### LIGHT AND DARK MODE ####
 
-  var table = $(".cell");
+  // Change between light and dark mode
+
+  var toggle_btn;
+  var all_content;
+
+  const main = document.querySelector("#fmain");
+
+  var dark_mode = false;
+
+  // Colors:
+
+  // Dark:
+
+  var bgc1 = "#1f1f1f";
+  var ac1 = "#bb86fc";
+
+  // Light:
+
+  var bgc2 = "#f5f5f5";
+  var ac2 = "##2fa3d9";
+
+  // Variables for later:
+
+  var generate;
+  var sort;
+
+  var table;
+
+  // #### CODE ####
+
+  function declare()
+  {
+    toggle_btn = document.querySelector(".toggle-btn");
+    all_content = document.querySelector(".all-content");
+
+    generate = $("#generate");
+    sort = $("#sort");
+
+    table = $(".table-cell");
+  }
+
+  declare();
+
+  function toggleAnimation()
+  {
+    if (permission)
+    {
+      dark_mode = !dark_mode;
+
+      let clone = all_content.cloneNode(true);
+
+      if (dark_mode)
+      {
+        clone.classList.remove("light");
+        clone.classList.add("dark");
+      }
+      else
+      {
+        clone.classList.remove("dark");
+        clone.classList.add("light");
+      }
+
+      clone.classList.add("newdiv");
+      main.appendChild(clone);
+
+      document.body.classList.add("stop-scrolling");
+
+      clone.addEventListener("animationend", () =>
+      {
+        all_content.remove();
+        clone.classList.remove("newdiv");
+
+        document.body.classList.remove("stop-scrolling");
+
+        // Reset the variables
+        declare();
+        events();
+      });
+    }
+  }
+
+  // #### GENERATE NEW ARRAY AND SORT ARRAY ####
+
   var permission = true;
 
   // Returns a random Integer number
@@ -15,12 +96,10 @@ $(document).ready(function()
   // Generates a new array with random numbers (from 1 to 10)
   function generate_new_array()
   {
-    if (permission == true)
-      for (var i = 0; i < table.length; i++)
-        table.eq(i).text(generateRandomInteger(10));
+    if (permission)
+    for (var i = 0; i < table.length; i++)
+    table.eq(i).text(generateRandomInteger(10));
   }
-
-  generate.click(generate_new_array);
 
   // Slows down for loop execution
   const sleep = (time) =>
@@ -31,7 +110,7 @@ $(document).ready(function()
   // Bubblesort algorithm that sorts the array
   const bubblesort = async () =>
   {
-    if (permission == true)
+    if (permission)
     {
       permission = false;
       var size = Number(table.length);
@@ -44,8 +123,28 @@ $(document).ready(function()
           {
             await sleep(1000);
 
-            table.eq(i).animate({backgroundColor: "#ff851b"}).animate({backgroundColor: "#808080"});
-            table.eq(j).animate({backgroundColor: "#ef5350"}).animate({backgroundColor: "#808080"});
+            if (dark_mode)
+            {
+              table.eq(i).animate({backgroundColor: ac1}).animate({backgroundColor: bgc1}, 'slow', function ()
+              {
+                $(this).removeAttr('style');
+              });
+              table.eq(j).animate({backgroundColor: ac1}).animate({backgroundColor: bgc1}, 'slow', function ()
+              {
+                $(this).removeAttr('style');
+              });
+            }
+            else
+            {
+              table.eq(i).animate({backgroundColor: ac2}).animate({backgroundColor: bgc2}, 'slow', function ()
+              {
+                $(this).removeAttr('style');
+              });
+              table.eq(j).animate({backgroundColor: ac2}).animate({backgroundColor: bgc2}, 'slow', function ()
+              {
+                $(this).removeAttr('style');
+              });
+            }
 
             var temp = table.eq(j).text();
 
@@ -54,11 +153,21 @@ $(document).ready(function()
           }
 
           if (i == (size - 2))
-            permission = true;
+          permission = true;
         }
       }
     }
+
   }
 
-  sort.click(bubblesort);
+  function events()
+  {
+    toggle_btn.addEventListener("click", toggleAnimation);
+
+    generate.click(generate_new_array);
+    sort.click(bubblesort);
+  }
+
+  events();
+
 });
